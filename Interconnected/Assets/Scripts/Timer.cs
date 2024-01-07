4284711ef45f8a55bda2590a5e0aa67306be01d6
firzamhmd <firzamuhammad43@gmail.com>
2024-01-07 14:47:41 +0700

    bikin timer

---
+++ Interconnected/Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] GlobalVariable globalVariable;
+
+    [SerializeField] TextMeshProUGUI textCurrentTimer;
+
+    [SerializeField] float curTimerValue;
+    [SerializeField] float[] timerTargetValue;
+
+    bool isTimerStop = false;
+
+    TimeSpan timerCount;
+
+    private void Start()
+    {
+        StartCoroutine(timerStartCount());
+    }
+
+    private void Update()
+    {
+        if (curTimerValue >= 86400) //24jam
+        {
+            curTimerValue = 0;
+            isTimerStop = true;
+            globalVariable.isGameOver = true;
+        }
+    }
+    IEnumerator timerStartCount() 
+    {
+        while (true) 
+        {
+            if (!isTimerStop) 
+            {
+                if (!globalVariable.isGameFinish || !globalVariable.isGameOver)
+                {
+                    curTimerValue += Time.deltaTime;
+                    timerCount = TimeSpan.FromSeconds(curTimerValue);
+                    string timer = timerCount.ToString("mm':'ss':'ff");
+                    textCurrentTimer.text = timer;
+                }
+            }
+            yield return null;
+        }
+    }
+}
