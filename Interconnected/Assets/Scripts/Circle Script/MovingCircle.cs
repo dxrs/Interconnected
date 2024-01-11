0988123c6b5b541b76dd56933b9307669b89d5c0
firzamhmd <firzamuhammad43@gmail.com>
2024-01-11 22:18:31 +0700

    update script moving circle

--- Interconnected/Assets/Scripts/Circle Script/MovingCircle.cs
+++ Interconnected/Assets/Scripts/Circle Script/MovingCircle.cs
@@ -21,11 +21,13 @@ public class MovingCircle : MonoBehaviour
 
     [SerializeField] SpriteRenderer circleSR;
 
+   [SerializeField] Rigidbody2D rb2D;
     int startWaypoint = 0;
 
     private void Awake()
     {
         if (movingCircle == null) { movingCircle = this; }
+       // rb2D = circle.GetComponent<Rigidbody2D>();
     }
 
     private void Start()
@@ -35,29 +37,42 @@ public class MovingCircle : MonoBehaviour
 
     private void Update()
     {
-        if (isMoving && !circleTriggertWithDoor) 
+        if (isMoving && !circleTriggertWithDoor)
         {
-            circle.transform.position = Vector2.MoveTowards(circle.transform.position,
-                waypoint[startWaypoint].transform.position,
-                movementSpeed * Time.deltaTime);
+            // Batasi kecepatan maksimum
+            if (rb2D.velocity.magnitude < movementSpeed)
+            {
+                Vector2 targetPosition = waypoint[startWaypoint].transform.position;
+
+                // Batasi kecepatan maksimum
+                Vector2 moveDirection = (targetPosition - (Vector2)circle.transform.position).normalized;
+                rb2D.MovePosition(rb2D.position + moveDirection * movementSpeed * Time.deltaTime);
+            }
 
             circleSR.color = Color.Lerp(circleSR.color, new Color(1, 1, 1), 3 * Time.deltaTime);
         }
-        else 
+        else
         {
             circleSR.color = Color.Lerp(circleSR.color, new Color(.5f, .5f, .5f), 1 * Time.deltaTime);
-        }
-        if (circle.transform.position == waypoint[startWaypoint].transform.position)
-        {
-            startWaypoint += 1;
+
+            // Berhenti ketika mencapai waypoint
+            rb2D.drag = 5f;
         }
 
-        if (startWaypoint == waypoint.Length && !globalVariable.isGameFinish) 
+        // Jika posisi objek mendekati waypoint, pindah ke waypoint berikutnya
+        if (Vector2.Distance(circle.transform.position, waypoint[startWaypoint].transform.position) < 0.1f)
         {
-            // starWaypoint = index terakhir dari waypoint.length
+            startWaypoint = (startWaypoint + 1) % waypoint.Length;
+            rb2D.drag = 0f;
+        }
+
+        if (startWaypoint == waypoint.Length && !globalVariable.isGameFinish)
+        {
+            // startWaypoint = index terakhir dari waypoint.length
             startWaypoint = waypoint.Length - 1;
         }
-        if (globalVariable.isGameFinish) 
+
+        if (globalVariable.isGameFinish)
         {
             circle.transform.position = circleTarget.transform.position;
         }
@@ -67,7 +82,7 @@ public class MovingCircle : MonoBehaviour
     {
         Vector2 startPos = circlePath.GetChild(0).position;
         Vector2 prevPos = startPos;
-        foreach(Transform waypoint in circlePath) 
+        foreach (Transform waypoint in circlePath)
         {
             Gizmos.DrawSphere(waypoint.position, .3f);
             Gizmos.DrawLine(prevPos, waypoint.position);
