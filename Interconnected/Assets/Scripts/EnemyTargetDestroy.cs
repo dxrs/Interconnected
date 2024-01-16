a7a961ec27c8bd5052bebc7c616454af4785f4d6
firzamhmd <firzamuhammad43@gmail.com>
2024-01-16 22:36:24 +0700

    update script ui ingame

--- Interconnected/Assets/Scripts/EnemyTargetDestroy.cs
+++ Interconnected/Assets/Scripts/EnemyTargetDestroy.cs
@@ -12,6 +12,7 @@ public class EnemyTargetDestroy : MonoBehaviour
 
     [SerializeField] int[] targetEnemyDestroyValue;
     [SerializeField] TextMeshProUGUI[] textTargetEnemyDestroy;
+    [SerializeField] TextMeshProUGUI textEnemyDestroy;
 
     private void Awake()
     {
@@ -22,6 +23,18 @@ public class EnemyTargetDestroy : MonoBehaviour
     {
         if (LevelStatus.levelStatus.levelID == 2)
         {
+            if (curValueEnemyDestroy == Mathf.Clamp(curValueEnemyDestroy, 0, targetEnemyDestroyValue[2])) 
+            {
+                textEnemyDestroy.text = "Enemy Destroyed " + curValueEnemyDestroy + " / " + targetEnemyDestroyValue[2].ToString();
+            }
+            if(curValueEnemyDestroy == Mathf.Clamp(curValueEnemyDestroy, targetEnemyDestroyValue[2], targetEnemyDestroyValue[1])) 
+            {
+                textEnemyDestroy.text = "Enemy Destroyed " + curValueEnemyDestroy + " / " + targetEnemyDestroyValue[1].ToString();
+            }
+            if (curValueEnemyDestroy == Mathf.Clamp(curValueEnemyDestroy, targetEnemyDestroyValue[1], targetEnemyDestroyValue[0]))
+            {
+                textEnemyDestroy.text = "Enemy Destroyed " + curValueEnemyDestroy + " / " + targetEnemyDestroyValue[0].ToString();
+            }
             for (int i = 0; i < textTargetEnemyDestroy.Length && i < targetEnemyDestroyValue.Length; i++)
             {
                 textTargetEnemyDestroy[i].text = "Destroy " + targetEnemyDestroyValue[i] + " Enemy";
